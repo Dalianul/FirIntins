3cc3c1427f784846f0a39f40b48d92223a764a0c
Ardelean Ruben Dalian <ardeleandalian@gmail.com>
2026-03-22 11:41:06 +0200

    feat(wishlist): add module links for wishlist↔customer and wishlist-item↔product

---
+++ apps/backend/src/links/wishlist-item-product.ts
@@ -0,0 +1,8 @@
+import { defineLink } from "@medusajs/framework/utils"
+import ProductModule from "@medusajs/medusa/product"
+import WishlistModule from "../modules/wishlist"
+
+export default defineLink(
+  WishlistModule.linkable.wishlistItem,
+  ProductModule.linkable.product
+)
